e3b7bc978e0762059be5a3ec8c741db507df2214
taokyone <antonsson.andreas@hotmail.com>
2023-09-04 09:15:16 +0200

    Initial Commit

---
+++ tre_i_radUITests/tre_i_radUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tre_i_radUITestsLaunchTests.swift
+//  tre_i_radUITests
+//
+//  Created by Andreas Antonsson on 2023-09-04.
+//
+
+import XCTest
+
+final class tre_i_radUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
